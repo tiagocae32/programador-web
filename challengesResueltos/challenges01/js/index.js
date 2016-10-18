ca63d29eb270bf039e5a3f68bc8a9dfdbc13e16d
mpelufito <m.pelufito@gmail.com>
2016-10-17 21:09:21 -0300

    html, css y js del challenge01

---
+++ challengesResueltos/challenges01/js/index.js
@@ -0,0 +1,16 @@
+$("document").ready(function() {
+
+    $("#nuevoNombreJQ").text(nuevoNombre);
+    $("#primerNumeroJQ").text(primerNumero);
+    $("#segundoNumeroJQ").text(segundoNumero);
+    $(".primerResultadoJQ").text(primerResultado);
+
+    $("#tercerNumeroJQ").text(tercerNumero);
+    $(".segundoResultadoJQ").text(segundoResultado);
+
+    $("#cuartoNumeroJQ").text(cuartoNumero);
+    $("#quintoNumeroJQ").text(quintoNumero);
+
+    $(".tercerResultadoJQ").text(tercerResultado);
+
+});
---
+++ challengesResueltos/challenges01/js/resuelto.js
@@ -0,0 +1,12 @@
+
+var nuevoNombre = prompt("ingresa tu Nombre");
+var primerNumero = prompt("ingresa el primer numero");
+var segundoNumero = prompt("ingresa el segundo numero");
+var primerResultado = primerNumero + segundoNumero;
+
+var tercerNumero = prompt("ingresa el tercer numero");;
+var segundoResultado = tercerNumero - primerResultado;
+
+var cuartoNumero = prompt("ingresa el cuarto numero");
+var quintoNumero = prompt("ingresa el quinto numero");
+var tercerResultado = (cuartoNumero * quintoNumero) / segundoResultado;
